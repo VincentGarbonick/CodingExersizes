3d88c2b663d442714783a8c424080e18ad58aa90
vinnie <vrg6@zips.uakron.edu>
2020-01-31 19:01:11 -0500

    add infinite while loop

--- myProject1.cpp
+++ myProject1.cpp
@@ -3,8 +3,14 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std; 
-const float RUNNING, BIKING, LIFTING, YOGA; //declaration and initialization of consts for METs
+using std::cin; 
+using std::cout;
+using std::setw;
+using std::left;
+using std::endl;
+using std::right;
+
+const float RUNNING = 10.0, BIKING = 8.0, LIFTING = 3.0, YOGA = 2.5; //declaration and initialization of consts for METs
 
 
 
@@ -13,40 +19,64 @@ const float RUNNING, BIKING, LIFTING, YOGA; //declaration and initialization of 
 
 int main(void)
 {
-    bool quitting = false; //this is the variable for quitting. if this is set to true, the loop exits 
+    bool quitting = false, validLoop = false, validMins = false, validWeight = false; //this is the variable for quitting. if this is set to true, the loop exits 
     int userChoice; //this stores the choice of the user 
+    float weight, minutes; //variables used in MET calculation 
+
+    //clears output buffer 
+    cout << endl;
+    //heading of menu system
+    cout << setw(68) << "__________________________________________________________________ " << endl;
+    cout << setw(68) << "|                  Welcome to Vinnie's Fitness Center              |" << endl;
+    cout << setw(68) << "|                          Activity System                         |" << endl;
+    cout << setw(68) << "|__________________________________________________________________|" << endl;
+    //options of menu system
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "   MAIN MENU" << right << setw(2) << "|" << endl;
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "1) Stationary Bike " << right << setw(2) << "|" << endl;
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "2) Treadmilll " << right << setw(2) << "|" << endl;
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "3) Weight Lifting " << right << setw(2) << "|" << endl;
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "4) Hatha Yoga " << right << setw(2) << "|" << endl;
+    cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "5) Quit " << right << setw(2) << "|" << endl;
+    //bottom of menu system
+    cout << setw(68) << "|__________________________________________________________________|" << endl;
+    cout << endl;
+
     
-    while(quitting != true)
+    //clears output buffer
+    //loops runs forever unless quitting is set to false
+    while(quitting == false)
     {
-        //clears output buffer 
-        cout << endl;
-        //heading of menu system
-        cout << setw(68) << "__________________________________________________________________ " << endl;
-        cout << setw(68) << "|                  Welcome to Vinnie's Fitness Center              |" << endl;
-        cout << setw(68) << "|                          Activity System                         |" << endl;
-        cout << setw(68) << "|__________________________________________________________________|" << endl;
-        //options of menu system
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "   MAIN MENU" << right << setw(2) << "|" << endl;
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "1) Stationary Bike " << right << setw(2) << "|" << endl;
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "2) Treadmilll " << right << setw(2) << "|" << endl;
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "3) Weight Lifting " << right << setw(2) << "|" << endl;
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "4) Hatha Yoga " << right << setw(2) << "|" << endl;
-        cout << left << setw(2) << "|" << setw(21) << " " << setw(43) << "5) Quit " << right << setw(2) << "|" << endl;
-        //bottom of menu system
-        cout << setw(68) << "|__________________________________________________________________|" << endl;
-        cout << endl;
 
+
+
+        //polls for user input 
+        cout << setw(2) <<  "Enter the workout that you wish to track, or end to exit: " << endl;
         cin >> userChoice; 
+        //input validation for user input 
+        if(isdigit(userChoice) && userChoice <= 5 && userChoice >= 1)
+        {
 
-        if(userChoice == 1){quitting = true;};
+            //under all if statements, all input is validated and held within an infinite while loop. It will lock you into the process until you input valid variables
+            if(userChoice == 1)
+            { 
+                cout << "poopy";   
+            };
+
+        }
+        
+        else
+        {
+            cout << setw(2) << "That is not valid input, please follow the menu carefully." << "\n" << endl; 
+        };
 
 
 
 
 
 
+        validLoop = false; //resets valid 
+        cin.ignore(); //flush output buffer one last time 
+    };
+    return 0; 
 
-
-    }
-return 0; 
-}
+};
